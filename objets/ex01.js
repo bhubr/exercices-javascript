381faf8960f2dfba6351fe31e0a16f25f7395db2
Benoît Hubert <benoit.hubert@wildcodeschool.fr>
2018-04-03 13:22:29 +0200

    Initial commit

---
+++ objets/ex01.js
@@ -0,0 +1,44 @@
+/*----------------------------------------------*
+ | OBJETS / Exercice 1 / Ecrire un objet
+ *----------------------------------------------*
+ |
+ | RAPPEL, un objet est un ensemble de
+ | propriétés, écrites sous forme de paires
+ | clé-valeur, le tout entre accolades:
+ |
+ |     const person = { name: 'John', age: 34 }
+ |
+ | On trouve toujours la clé suivie de : puis de
+ | la valeur. Les paires clé-valeur sont séparées
+ | entre elles par des virgules.
+ |
+ | Autre exemple où une des clés est mise entre
+ | quotes car elle contient des caractères autres
+ | que lettre, chiffre, _ ou $
+ |
+ |     const city = {
+ |       name: 'Toulouse',
+ |       country: 'France',
+ |       'post-code': '31000'
+ |     }
+ |
+ *----------------------------------------------*
+ |
+ | ENONCE:
+ | En partant de l'objet movie vide ci-dessous,
+ | ajouter quatre propriétés à l'objet movie:
+ |   name          contenant "Star Wars Episode IV"
+ |   genre         contenant "Science-fiction"
+ |   release-year  contenant 1977
+ |   main-actors   contenant un tableau de 3 chaines:
+ |                   "Mark Hamill", "Carrie Fisher", "Harrison Ford"
+ */
+
+
+const movie = {
+}
+
+
+
+// Ne pas toucher ceci
+module.exports = movie
