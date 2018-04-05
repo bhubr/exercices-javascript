6433d9bc2811a1d46298a0c7225a66430c27c732
Benoît Hubert <benoit.hubert@wildcodeschool.fr>
2018-04-05 06:18:42 +0200

    2ème exo sur map()

---
+++ tableaux/ex02.js
@@ -0,0 +1,33 @@
+/*----------------------------------------------*
+ | TABLEAUX / Exercice 2 / Suite avec map()
+ *----------------------------------------------*
+ |
+ | ENONCE
+ | Ecrire une fonction nomsComplets, qui à partir
+ | d'un tableau d'objets contenant chacun un prénom
+ | et un nom, va renvoyer un tableau de noms complets
+ | (nom + prénom)
+ */
+
+
+// --------------- Code à écrire/modifier --------------
+function nomsComplets() {
+}
+
+
+
+// Exemple d'utilisation
+const personnagesGoT = [
+  { prenom: 'Jon', nom: 'Snow' },
+  { prenom: 'Arya', nom: 'Stark' },
+  { prenom: 'Tyrion', nom: 'Lannister' }
+]
+// Attendu : [ 'Jon Snow', 'Arya Stark', 'Tyrion Lannister' ]
+console.log(nomsComplets(personnagesGoT))
+
+
+// -----------------------------------------------------
+
+module.exports = {
+	nomsComplets
+}
---
+++ tests/tableaux/ex02.test.js
@@ -0,0 +1,22 @@
+const assert = require('assert')
+const checks = require('../../lib/checks')
+const { nomsComplets } = require('../../tableaux/ex02')
+
+describe('Objets - Exo 2 - map()', () => {
+
+  it("nomsComplets est une fonction", () => {
+    assert.equal(typeof nomsComplets, 'function')
+  })
+
+  it("nomsComplets renvoie les valeurs attendues", () => {
+    const tableauTest = [
+      { prenom: 'David', nom: 'Bowie'},
+      { prenom: 'John', nom: 'Lennon' }
+    ]
+    assert.deepEqual(
+      nomsComplets(tableauTest),
+      ['David Bowie', 'John Lennon']
+    )
+  })
+
+})
