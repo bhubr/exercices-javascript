63531bc6b8f0ce1cee1c31155f002fc28ce98983
Benoît Hubert <benoithubert@gmail.com>
2018-04-08 22:47:45 +0200

    Make tableaux/ex02 universal

--- tableaux/ex02.js
+++ tableaux/ex02.js
@@ -28,6 +28,6 @@ console.log(nomsComplets(personnagesGoT))
 
 // -----------------------------------------------------
 
-module.exports = {
-	nomsComplets
+if(typeof global !== 'undefined') {
+  module.exports = nomsComplets
 }
