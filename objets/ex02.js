d993a9a3d32469d673901e4c5954f8cb3506c76e
Benoît Hubert <benoithubert@gmail.com>
2018-04-09 21:02:47 +0200

    fix ex02 et tests

--- objets/ex02.js
+++ objets/ex02.js
@@ -30,7 +30,7 @@
  | La fonction doit ajouter à CHAQUE objet du tableau
  | la propriété donnée par les arguments clé et valeur.
  | Elle doit renvoyer le tableau.
- | TU PEUX UTILISER setObjectProp écrite dans l'exercice
+ | TU PEUX COPIER-COLLER setObjectProp écrite dans l'exercice
  | précédent (elle est inclue ici avec un require, pas
  | besoin de copier coller)
  | 
@@ -45,8 +45,11 @@
  */
 
 // PAS TOUCHE
-const setObjectProp = require('./ex01').setObjectProp
-const movies = require('./ressources/ex02movies')()
+const movies = [
+  { name: 'Star Wars IV: A New Hope' },
+  { name: 'Star Wars V: Empire Strikes Back' },
+  { name: 'Star Wars VI: Return Of The Jedi' }
+]
 console.log(movies)
 
 // ------- Code à écrire / modifier ci-dessous -------
