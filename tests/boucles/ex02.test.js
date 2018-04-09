e45f3e80984efb756fd4ee4d8ff5e2e232168f67
Benoît Hubert <benoithubert@gmail.com>
2018-04-09 23:16:37 +0200

    fix tests&mocha bootstrap

--- tests/boucles/ex02.test.js
+++ tests/boucles/ex02.test.js
@@ -1,13 +1,12 @@
-const assert = require('assert')
-const catchConsole = require('../../lib/catchConsole')
-
+if(typeof global !== 'undefined') {
+  catchConsole.enable(true)
+  require('../../boucles/ex02')
+  catchConsole.disable()
+}
 describe('Boucles - Exo 2 - for', () => {
 
   it("affiche les nombres de 5 à 0", () => {
-    catchConsole.enable()
-    require('../../boucles/ex02')
-    const consoleOutput = console.getCatched()
-    catchConsole.disable()
+    const consoleOutput = catchConsole.get()
     const actual = consoleOutput.join('\n')
     const expected = '5\n4\n3\n2\n1\n0'
     const expectedWithIndent = [5, 4, 3, 2, 1, 0].map(l => '      ' + l)
