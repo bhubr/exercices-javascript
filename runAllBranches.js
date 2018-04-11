3294a64c81033106309300cdb737718796a97378
Benoît Hubert <benoithubert@gmail.com>
2018-04-11 02:08:28 +0200

    split runners

---
+++ runAllBranches.js
@@ -0,0 +1,20 @@
+const Git = require("nodegit")
+const runAllTests = require('./runAllTests')
+const students = require('./students.json')
+
+const checkoutBranch = branchName => Git.Repository.open(".")
+.then(repo => repo.getBranch(`refs/heads/${branchName}`)
+  .then(ref => repo.checkoutRef(ref))
+)
+
+const runAllBranches = () =>
+  Promise.reduce(students, (results, stud) => {
+    const { name, branchName } = stud
+    console.log(`Running tests for ${name} => checkout branch ${branchName}`)
+    return checkoutBranch(branchName)
+    .then(runAllTests)
+    .then(tests => results.concat([{ name, branchName, tests }]))
+  })
+
+runAllBranches()
+.then(console.log)
--- runAllTests.js
+++ runAllTests.js
@@ -1,25 +1,12 @@
 #!/usr/local/bin/node
-// TEST=$1
-// mocha --require tests/bootstrap.js $TEST
 
 const glob = require('glob-fs')()
 const Promise = require('bluebird')
 const { exec } = require('child_process')
 const execAsync = Promise.promisify(exec)
-const Git = require("nodegit")
-// const students = require('./students.json')
 
-const checkoutBranch = branchName => Git.Repository.open(".")
-.then(repo => repo.getBranch(`refs/heads/${branchName}`)
-  .then(ref => repo.checkoutRef(ref))
-)
-
-
+// Get test command
 const getCmd = test => `mocha --require tests/bootstrap.js tests/${test.replace('.js', '.test.js')}`
-
-const files = glob.readdirSync('**/ex*.js')
-  .filter(f => /ex\d+\.js$/.test(f))
-  // .map(f => 'tests/' + f.replace('.js', '.test.js'))
 
 const log = (f, status) => {
   console.log(`${f}: ${ status ? 'OK': 'Fail' }`)
@@ -30,21 +17,14 @@ const runTest = f => execAsync(getCmd(f))
 .then(() => log(f, true))
 .catch(() => log(f, false))
 
-const runAllTests = () => Promise.reduce(
-  files, (c, file) => runTest(file)
-    .then(status => c.concat([{ file, status }])),
-  []
-)
+const runAllTests = () => 
+  glob.readdirPromise('**/ex*.js')
+  .then(files => files.filter(f => /ex\d+\.js$/.test(f)))
+  .then(files => Promise.reduce(
+      files, (c, file) => runTest(file)
+        .then(status => c.concat([{ file, status }])),
+      []
+    )
+  )
 
-// const runOnAllBranches =
-
-// Promise.reduce(students, (results, stud) => {
-//   const { name, branchName } = stud
-//   console.log(`Running tests for ${name} => checkout branch ${branchName}`)
-//   return checkoutBranch(branchName)
-//   .then(runAllTests)
-//   .then(tests => results.concat([{ name, branchName, tests }]))
-// })
-
-runAllTests()
-.then(console.log)
+module.exports = runAllTests
