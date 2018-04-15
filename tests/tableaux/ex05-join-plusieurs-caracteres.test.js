036a6c795737d87095a2b1792d9a5c2460e97544
Benoît Hubert <benoithubert@gmail.com>
2018-04-15 16:40:34 +0200

    fix tableaux/ex05 test (minor)

--- tests/tableaux/ex05-join-plusieurs-caracteres.test.js
+++ tests/tableaux/ex05-join-plusieurs-caracteres.test.js
@@ -8,10 +8,10 @@ if(typeof global !== 'undefined') {
 describe('Tableaux - Exo 5 - Join avec séparateur/glue à plus d\'un caractère', () => {
 
   it("- albumsString contient les noms d\'albums, joints avec le bon séparateur", () => {
-    assert.ok(albumsString === 'Blue <> Continuum <> Is This It <> Definitely Maybe')
+    assert.equal(albumsString, 'Blue <> Continuum <> Is This It <> Definitely Maybe')
   })
 
   it('- moviesString contient les noms des films, un sur chaque ligne, préfixé de "* " ', () => {
-    assert.ok(`* ${moviesString}` === '* The Matrix\n* Interstellar\n* Avatar\n* Alien')
+    assert.equal(`* ${moviesString}`, '* The Matrix\n* Interstellar\n* Avatar\n* Alien')
   })
 })
